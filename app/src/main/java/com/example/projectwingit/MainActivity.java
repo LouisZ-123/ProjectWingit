ad6d52e22f070f56ac88a7c471725b2e97ee67d6
CSE110Wingit <73203980+CSE110Wingit@users.noreply.github.com>
2020-11-20 01:17:45 -0800

    Changed User Account fragment to Activity

--- app/src/main/java/com/example/projectwingit/MainActivity.java
+++ app/src/main/java/com/example/projectwingit/MainActivity.java
@@ -9,10 +9,13 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
@@ -48,7 +51,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
-
     @Override
     public void onBackPressed() {
         if (dl.isDrawerOpen(GravityCompat.START)) {
@@ -70,7 +72,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.account_button_top:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, new AccountOptions()).commit();
+                Intent intent = new Intent(this, UserAccount.class);
+                startActivity(intent);
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -104,22 +107,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         switch (item.getItemId()) {
             case R.id.hamburger_home:
-                fragment = new HomeFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
                 break;
             case R.id.hamburger_create_recipe:
-                fragment = new CreateRecipe();
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new CreateRecipe()).commit();
                 break;
             case R.id.hamburger_local_venues:
-                fragment = new LocalVenues();
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new LocalVenues()).commit();
                 break;
             case R.id.hamburger_account_options:
-                fragment = new AccountOptions();
+                Intent intent = new Intent(this, UserAccount.class);
+                startActivity(intent);
                 break;
             case R.id.hamburger_settings:
-                fragment = new Settings();
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new Settings()).commit();fragment = new Settings();
                 break;
         }
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
         dl.closeDrawer(GravityCompat.START);
         return true;
     }
