b85326527b3b258a131fb36fe3124e8a7df238dc
CSE110Wingit <73203980+CSE110Wingit@users.noreply.github.com>
2020-11-20 20:13:46 -0800

    Created user account initial page
    user account page uses navigation view to show the items

--- app/src/main/java/com/example/projectwingit/UserAccount.java
+++ app/src/main/java/com/example/projectwingit/UserAccount.java
@@ -1,14 +1,34 @@
 package com.example.projectwingit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class UserAccount extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class UserAccount extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private NavigationView accountView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_account);
+
+        NavigationView accountView = findViewById(R.id.userAccountView);
+        accountView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+//        switch (item.getItemId()) {
+//
+//        }
+
+        return false;
     }
 }
