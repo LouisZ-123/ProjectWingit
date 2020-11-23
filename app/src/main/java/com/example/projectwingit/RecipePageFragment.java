bcd5511a212e38e006c8dc590b0048604581bc25
henry lister <hlister@ucsd.edu>
2020-11-23 14:07:51 -0800

    Recipe page and rating dialog

--- app/src/main/java/com/example/projectwingit/RecipePageFragment.java
+++ app/src/main/java/com/example/projectwingit/RecipePageFragment.java
@@ -7,15 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.android.material.card.MaterialCardView;
+import android.widget.Button;
+import android.app.Dialog;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link Settings#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class RecipePageFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,19 +26,12 @@ public class HomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private MaterialCardView card1;
+    private Button rateButton;
+    Dialog rateDialog;
 
-    public HomeFragment() {
+    public RecipePageFragment() {
         // Required empty public constructor
     }
-
-    // button for the first card that sends you to the recipe page
-   /* private View.OnClickListener cardListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            getFragmentManager().beginTransaction().replace(R.id.container, new RecipePageFragment()).commit();
-        }
-    };*/
 
     /**
      * Use this factory method to create a new instance of
@@ -46,11 +39,11 @@ public class HomeFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment Settings.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
+    public static RecipePageFragment newInstance(String param1, String param2) {
+        RecipePageFragment fragment = new RecipePageFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,23 +58,44 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_recipe_page, container, false);
 
-        // set the button to switch fragments
-        // TODO replace button code using recyclerView which can make a button for all cards
-        card1 = v.findViewById(R.id.card);
-        card1.setOnClickListener(new View.OnClickListener() {
+        // rating dialog characteristics TODO maybe make the rating dialog its own method
+        rateDialog = new Dialog(getActivity());
+        rateDialog.setContentView(R.layout.rating_dialog);
+        rateDialog.setCancelable(false);
+
+        Button cancel = rateDialog.findViewById(R.id.cancel_dialog_button);
+        Button okay = rateDialog.findViewById(R.id.ok_dialog_button);
+
+        cancel.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                getFragmentManager().beginTransaction().replace(R.id.container, new RecipePageFragment()).commit();
+                rateDialog.dismiss();
             }
         });
+
+        okay.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                rateDialog.dismiss();
+            }
+        });
+
+        // rating button characteristics
+        rateButton = (Button) v.findViewById(R.id.ratebutton);
+        rateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rateDialog.show();
+            }
+        });
+
 
         // Inflate the layout for this fragment
         return v;
